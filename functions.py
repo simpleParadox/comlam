Fixed averaging method.

functions.py
@@ -10,11 +10,14 @@ def get_avg_trs():
     pass
     
 
-def store_avg_tr(participant, nifti_path):
+def store_avg_tr(participant, nifti_patha, nifti_pathb):
 
+    # participant = 1003
+    # nifti_patha = "smb://localhost/Google Drive/My Drive/CoMLaM_rohan/CoMLaM/Preprocessed/Reg_to_Std_and_Str/P_0999/Synonym_RunA_12.feat/filtered_func_data.nii"
+    # nifti_pathb = "smb://localhost/Google Drive/My Drive/CoMLaM_rohan/CoMLaM/Preprocessed/Reg_to_Std_and_Str/P_0999/Synonym_RunB_13.feat/filtered_func_data.nii"
     # Now avg the TRs for each stimuli and for each corresponding 'run' and for each stimuli.
     tr_meta_path = "E:\My Drive\CoMLaM_rohan\CoMLaM\\" + str(participant) + "_TRsToUse.xlsx"
-    metadata = pd.read_excel(tr_meta_path) # Remove this in the final version.
+    metadata = pd.read_excel(tr_meta_path)  # Remove this in the final version.
 
 
     stims =  metadata.iloc[:,[0,2,7]].values
@@ -23,18 +26,24 @@ def store_avg_tr(participant, nifti_path):
 
     #---------------------------------------------------------------
     # Now retrieve from the nifti file.
-    np_a = img.get_data(nifti_path)
+    np_a = img.get_data(nifti_patha)
+    np_b = img.get_data(nifti_pathb)
 
     # Now preprocess the nifti and store it later in a directory called 'avg_tr_nifti' on GDrive.
     np_a = np.transpose(np_a, (3, 0, 1, 2))
-
     np_a_rs = np_a.reshape(np_a.shape[0], -1)
-
     mean_voxels_a = np.mean(np_a_rs, axis=0)
+
+    np_b = np.transpose(np_b, (3, 0, 1, 2))
+    np_b_rs = np_b.reshape(np_b.shape[0], -1)
+    mean_voxels_b = np.mean(np_b_rs, axis=0)
 
     # Removing the mean voxel value.
     for i, row in enumerate(np_a_rs):
         np_a_rs[i] = np_a_rs[i] - mean_voxels_a
+
+    for i, row in enumerate(np_b_rs):
+        np_b_rs[i] = np_b_rs[i] - mean_voxels_b
 
 
     # Now map the nifti indexes in the TRsToUse excel file to the fmri data.
@@ -50,7 +59,7 @@ def store_avg_tr(participant, nifti_path):
         if stim[1] == 1:
             run1_nifti[stim[0]].append(np_a_rs[stim[2]])
         else:
-            run2_nifti[stim[0]].append(np_a_rs[stim[2]])
+            run2_nifti[stim[0]].append(np_b_rs[stim[2]])
 
     # Now average the TRs.
     run1_nifti_avg = {}
@@ -67,14 +76,13 @@ def store_avg_tr(participant, nifti_path):
         # Make sure run1 and run2 exists for all the two-word stims.
         if key in run1_nifti_avg.keys() and key in run2_nifti_avg.keys():
             if type(run1_nifti_avg[key]) == np.ndarray and type(run2_nifti_avg[key]) == np.ndarray:
-                # print(key)
-                tr_run1 = np.reshape(run1_nifti_avg[key],(-1, run1_nifti_avg[key].shape[0]))
-                tr_run2 = np.reshape(run2_nifti_avg[key],(-1, run2_nifti_avg[key].shape[0]))
+                tr_run1 = np.reshape(run1_nifti_avg[key], (-1, run1_nifti_avg[key].shape[0]))
+                tr_run2 = np.reshape(run2_nifti_avg[key], (-1, run2_nifti_avg[key].shape[0]))
                 concat_TR[key] = np.concatenate((tr_run1, tr_run2), axis=1)
 
 
     # Now store the values.
-    np.savez_compressed(f'E:\My Drive\CoMLaM_rohan\CoMLaM\\avg_trs_concat\Synonym_RunB\\P_{participant}B.npz', concat_TR)
+    np.savez_compressed(f'E:\My Drive\CoMLaM_rohan\CoMLaM\\Synonym_RunB\\P_{participant}B.npz', concat_TR)
 
     # Load like this.
     # temp = np.load('E:\My Drive\CoMLaM_rohan\CoMLaM\\avg_trs_concat\\P_1003.npz', allow_pickle=True)['arr_0'].tolist()
@@ -114,9 +122,40 @@ def load_nifti_and_w2v(participant, synonym_condition):
         x_data.append(fmri)
         y_data.append(w2v_data[stim])
 
-    return x_data, y_data
+    stims = []
+    for stim in nifti_data.keys():
+        stims.append(stim)
+
+    return x_data, y_data, stims
 
 
+def extended_2v2(y_test, preds):
+    """
+    There are two additions to this function over the previous two_vs_two test.
+    1. The grid figures will be symmetric now.
+    2. Each pair of words is compared only once.
+    """
+    points = 0
+    total_points = 0
+    n_words = 12
+
+    for i in range(preds.shape[0] - 1):
+        s_i = y_test[i]
+        s_i_pred = preds[i]
+        for j in range(i + 1, preds.shape[0]):
+            temp_score = 0
+            s_j = y_test[j]
+            s_j_pred = preds[j]
+
+            dsii = cosine(s_i, s_i_pred)
+            dsjj = cosine(s_j, s_j_pred)
+            dsij = cosine(s_i, s_j_pred)
+            dsji = cosine(s_j, s_i_pred)
+
+            if dsii + dsjj <= dsij + dsji:
+                points += 1
+                temp_score = 1  # If the 2v2 test does not pass then temp_score = 0
+            total_points += 1
 
 
 def load_stim_vectors(participant):

main.py
@@ -15,13 +15,12 @@ from sklearn.model_selection import train_test_split
 def avg_trs():
     # Function to store Averaged concatenated TRs on GDrive.
     nifti_path = "E:\My Drive\CoMLaM_rohan\CoMLaM\Preprocessed\Reg_to_Std_and_Str\\"
-    tr_meta_path = "E:\My Drive\CoMLaM_rohan\CoMLaM\\"
     participants = [1003, 1004, 1006, 1007, 1008, 1010, 1012, 1013, 1016, 1017, 1019]
     for participant in participants:
-        print(participant)
         file_name = nifti_path + "P_" + str(participant) + "\\"
-        for file in glob.glob(file_name + "Synonym_RunB*\\filtered_func_data.nii"):
-            store_avg_tr(participant, file)
+        file_name_a = glob.glob(file_name + "Synonym_RunA*\\filtered_func_data.nii")
+        file_name_b = glob.glob(file_name + "Synonym_RunB*\\filtered_func_data.nii")
+        store_avg_tr(participant, file_name_a, file_name_b)
 
 
 def create_w2v_mappings():
@@ -54,7 +53,12 @@ def create_w2v_mappings():
     np.savez_compressed('G:\comlam\embeds\\two_words_stim_w2v_concat_dict.npz', stim_vector_dict)
 
 
-def ridge(part, synonym_condition):
+def cross_validation_nested(part, synonym_condition):
+    """
+    :param part: Accepts a list of participants. Example: [1003, 1006]
+    :param synonym_condition: [The synonym task. Accepts 'A' or 'B']
+    :return: 2v2 accuracy for the participant.
+    """
     # Do ridge regression with GridSearchCV here.
     # Run the analysis for each participant here.
 
@@ -68,6 +72,8 @@ def ridge(part, synonym_condition):
         # Load the data and the stims to do a leave two out cv.
         # Load the nifti, the word vectors, and the stim and then leave out two samples on which you'll do 2v2.
 
-        # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+
 
 